deac2ac97323d8024c74b05948a29156bffc11fd
Siddharth <siddharthmishra10e@gmail.com>
2025-11-10 04:26:02 +0530

    getting json response

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
-console.log("Coffee and Backend")
+
+const GithubError = {
+  "message": "Not Found",
+  "documentation_url": "www.google.com",
+  "status": "404"
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -23,4 +30,6 @@ app.get('/terms', (req, res) => {
   res.send("<h2>Mutual funds are subject to market risks!</h2>")
 })
 
-
+app.get('/github', (req, res) => {
+  res.json(GithubError)
+})
